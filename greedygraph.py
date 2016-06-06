1e2d99364ef9d0d297422c187257272b82703bdf
xinjie <capitalg_31@163.com>
2016-06-06 17:21:45 +0800

    now i have a moving dot

--- greedygraph.py
+++ greedygraph.py
@@ -3,7 +3,7 @@ from copy import deepcopy
 from random import randrange
 import time
 import os
-from inputwithoutwait import getch
+# from inputwithoutwait import getch    # getch don't work well on Pycharm
 class GreedySnake:
     def __init__(self, pos_x, pos_y, pat='o'):     # 方向是否合法交给controler控制
         self.__len = 1
@@ -24,22 +24,22 @@ class GreedySnake:
 
     def move_right(self):
         self.__body.pop()
-        self.__head.x += 1
+        self.__head.y += 1
         self.__body.append(deepcopy(self.__head))
 
     def move_left(self):
         self.__body.pop()
-        self.__head.x -= 1
+        self.__head.y -= 1
         self.__body.append(deepcopy(self.__head))
 
     def move_up(self):
         self.__body.pop()
-        self.__head.y -= 1
+        self.__head.x -= 1
         self.__body.append(deepcopy(self.__head))
 
     def move_down(self):
         self.__body.pop()
-        self.__head.y += 1
+        self.__head.x += 1
         self.__body.append(deepcopy(self.__head))
 
     def __str__(self):
@@ -75,29 +75,34 @@ class DirectionError(Exception):
 
 class GreedyController:
     direct = {'w': 'up', 's': 'down', 'a': 'left', 'd': 'right'}
+    snake_move = {}
     def __init__(self, the_snake, init_direction='right'):
-        self.__cur_direction = init_direction
+        assert isinstance(the_snake, GreedySnake)
+        self.cur_direction = init_direction
         self.__snake = the_snake
+        GreedyController.snake_move = {'up': self.__snake.move_up, 'down': self.__snake.move_down, 'left': self.__snake.move_left, 'right': self.__snake.move_right}
 
     @property
-    def cur_direction(self):
+    def cur_direction(self): # cur_direction: 'up', 'down', 'right', 'left'
         return self.__cur_direction
 
     @cur_direction.setter
     def cur_direction(self, direction):
-        if direction not in GreedyController.direct.keys():
+        if direction not in GreedyController.direct.values():
             raise DirectionError
         else:
-            self.__cur_direction = GreedyController.direct[direction]
+            self.__cur_direction = direction
 
     def turn(self, direction):
-        if GreedyController.direct[direction] == self.cur_direction:
-            pass
+        if self.cur_direction == GreedyController.direct.get(direction, None):
+            GreedyController.snake_move[self.cur_direction]()
         else:
             try:
-                self.cur_direction == GreedyGraph.direct[direction]
+                self.cur_direction = GreedyController.direct[direction]
             except DirectionError as e:
                 print(e, 'direcion should be in w,s,a,d')
+            finally:
+                GreedyController.snake_move[self.cur_direction]()
 
 class GreedyApp:
     def __init__(self, length, width):
@@ -110,14 +115,17 @@ class GreedyApp:
     def run(self):
         self.__graph.paint(self.__snake)
         while True:
-
+            # input_key = getch()
+            input_key = input()
+            self.__controller.turn(input_key[0])
             os.system('clear')
-
+            self.__graph.paint(self.__snake)
 
 
 
 
 if __name__ == '__main__':
+    """
     length = 30
     width = 30
     graph = GreedyGraph(length, width)
@@ -139,4 +147,8 @@ if __name__ == '__main__':
     print('-'*width)
 
     controller = GreedyController(snake)
+    """
+    app = GreedyApp(30, 30)
+    app.run()
+
 
